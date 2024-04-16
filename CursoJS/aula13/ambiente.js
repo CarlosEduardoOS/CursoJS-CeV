de379e1ebd8224abc87a9fdb08a98ebf63a5a634
Carlos Eduardo <carloseducontatodev@gmail.com>
2024-04-16 10:06:19 -0300

    terminei aula 13

---
+++ CursoJS/aula13/ambiente.js
@@ -0,0 +1,12 @@
+var numero = 1
+/*
+while(numero <= 500) {
+    console.log(`Passo ${numero}`)  // ESTRUTURA DE REPETIÇÂO COM TESTE LOGICO EM CIMA
+    numero++
+}
+*/
+
+do {
+    console.log(`Passo ${numero}`) // ESTRUTURA DE REPETIÇÂO COM TESTE LOGICO EM BAIXO
+    numero++
+} while(numero <= 500)
--- CursoJS/aula13/node_762143b3c20b5.tmp.js
+++
@@ -1,5 +0,0 @@
-var numero = 1
-while(numero <= 100000000000000) {
-    console.log(numero)
-    numero++
-}
